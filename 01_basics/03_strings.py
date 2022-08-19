006b1c59310f18dbfc60ea65adef023806fcdd6a
Tushar R. Mahore <54020003+mahoretushar@users.noreply.github.com>
2022-08-19 19:37:40 +0530

    Day 4

--- 01_basics/03_strings.py
+++ 01_basics/03_strings.py
@@ -1,6 +1,37 @@
-# Single Line Comment
-"""
-This is a multiline comment in Python
-"""
+# Strings
 
-# This is a new line
+# Dynamically Typed and Staticaly Typed
+
+course_name_1 = "Python Programming"
+course_name_2 = '      Python Programming'
+
+message = """Hi I am Batman 
+& I am a Suprehero
+& I am friends with Superman"""
+
+# print(course_name_1)
+# print(course_name_2)
+# print(message)
+
+
+# function & method
+
+string_length = len(course_name_1)
+print(string_length)
+
+# indexing
+print(course_name_1[0])
+# [start:end]
+print(course_name_1[0:4])
+print(course_name_1[:4])
+print(course_name_1[:])
+
+# -ve indexing
+print(course_name_1[-1])
+print(course_name_1[-3:-1])
+
+# [start:end:step]
+print(course_name_1[::2])
+
+# print a string in reverse order
+print(course_name_1[::-1])
---
+++ 01_basics/04_escape_sequences.py
@@ -0,0 +1,8 @@
+# \"
+# \'
+# \\
+# \n
+# \t
+
+course_name = "Python is the \"Best\" Programming Lang"
+print(course_name)
