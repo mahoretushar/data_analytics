037f0bbd2bf3fa9bcbdd89ce08e7e56d12df3765
Tushar R. Mahore <54020003+mahoretushar@users.noreply.github.com>
2022-08-22 19:54:25 +0530

    day 6

---
+++ 02_control_flow/01_comparison_operators.py
@@ -0,0 +1,21 @@
+""" Building Blocks
+        - data types & data structures
+        - conditions (if, else)
+        - loops (for, while)
+        - functions 
+"""
+
+# print(10 > 3)       # True
+# print(10 >= 3)      # True
+# print(10 < 3)       # False
+# print(10 <= 3)      # False
+# print(10 <= 10)     # True
+# print(10 == 3)      # False
+# print(10 == '10')   # False 
+# print(10 != '10')   # True 
+
+# we can use comparision operators with strings
+print('bag' > 'apple')      # True
+print('bag' > 'BAG')        # True
+print(ord('B'))             # Return ASCII Value
+print(ord('b'))
