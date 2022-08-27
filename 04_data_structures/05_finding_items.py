daf4b9e95d252e49c7d96543fd10c1b6407f870b
Tushar R. Mahore <54020003+mahoretushar@users.noreply.github.com>
2022-08-27 20:05:18 +0530

    day_11

---
+++ 04_data_structures/05_finding_items.py
@@ -0,0 +1,16 @@
+letters = ['a', 'd', 'b', 'c', 'd', 'e', 1, 2, 3, 'd']
+
+# find index of 'b'
+print(letters.index(2))
+
+# print(letters.index('z'))   # generates an error
+
+
+# in keyword is used for checking the existence
+if 'z' in letters:
+    print(letters.index('z'))
+else:
+    print("Letter not Found")
+
+# to check total number of occurences
+print(f"Number of d's in list: {letters.count('d')}")
---
+++ 04_data_structures/06_sorting_elements.py
@@ -0,0 +1,8 @@
+numbers = [3, 51, 2, 8, 16]
+
+# to sort the list
+# sorted = numbers.sort()
+# print(sorted)
+
+print(sorted(numbers))
+
